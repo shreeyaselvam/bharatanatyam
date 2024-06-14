f39fc7c4ca8d791aa563e980244606d19d1a1244
Surya Shanmugaselvam <amy6zx@virginia.edu>
2024-06-13 22:58:22 -0400

    Committing slideshow

--- src/App.js
+++ src/App.js
@@ -1,23 +1,98 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import logo from './assets/logofile.png';
+import Slideshow from './Components/Slideshow';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Offcanvas from 'react-bootstrap/Offcanvas';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" data-bs-theme="dark">
+      {/* <nav class="navbar navbar-expand-sm navbar-custom fixed-top">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">
+            <img src={logo} alt="Avatar Logo" class="rounded-pill logo"/> 
+          </a>
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link" href="#"><i class="fa-solid fa-house"></i></a>
+            </li>
+            <li class="nav-item">
+              <div class="dropdown">
+                <a class="nav-link" data-bs-toggle="dropdown" href="#">Arangatram</a>
+                <ul class="dropdown-menu" style={{marginTop: '25px'}}>
+                  <li><a class="dropdown-item" href="#">Invitation</a></li>
+                  <li><a class="dropdown-item" href="#">Brochure</a></li>
+                  <li><a class="dropdown-item" href="#">Livestream</a></li>
+                  <li><hr class="dropdown-divider"/></li>
+                  <li><a class="dropdown-item" href="#">Guru & Sishya</a></li>
+                  <li><a class="dropdown-item" href="#">Orchestra</a></li>
+                </ul>
+              </div>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Performances</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Mission</a>
+            </li>
+          </ul>
+        </div>
+      </nav> */}
+      <>
+
+        <Navbar key={'sm'} expand={'sm'} className="bg-body-tertiary mb-3">
+          <Container fluid>
+            <Navbar.Brand href="#">Navbar Offcanvas</Navbar.Brand>
+            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${'sm'}`} />
+            <Navbar.Offcanvas
+              id={`offcanvasNavbar-expand-${'sm'}`}
+              aria-labelledby={`offcanvasNavbarLabel-expand-${'sm'}`}
+              placement="end"
+            >
+              <Offcanvas.Header closeButton>
+                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${'sm'}`}>
+                  Offcanvas
+                </Offcanvas.Title>
+              </Offcanvas.Header>
+              <Offcanvas.Body>
+                <Nav className="justify-content-end flex-grow-1 pe-3">
+                  <Nav.Link href="#action1">Home</Nav.Link>
+                  <Nav.Link href="#action2">Link</Nav.Link>
+                  <NavDropdown
+                    title="Dropdown"
+                    id={`offcanvasNavbarDropdown-expand-${'sm'}`}
+                  >
+                    <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                    <NavDropdown.Item href="#action4">
+                      Another action
+                    </NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="#action5">
+                      Something else here
+                    </NavDropdown.Item>
+                  </NavDropdown>
+                </Nav>
+                <Form className="d-flex">
+                  <Form.Control
+                    type="search"
+                    placeholder="Search"
+                    className="me-2"
+                    aria-label="Search"
+                  />
+                  <Button variant="outline-success">Search</Button>
+                </Form>
+              </Offcanvas.Body>
+            </Navbar.Offcanvas>
+          </Container>
+        </Navbar>
+    </>
+      <Slideshow />
     </div>
   );
 }
---
+++ src/Components/Slideshow.js
@@ -0,0 +1,39 @@
+import '../App.css';
+import Carousel from 'react-bootstrap/Carousel';
+import pic1 from '../assets/AbhayaKaram.jpg';
+import pic2 from '../assets/CherryBlossomImage.jpg';
+import pic3 from '../assets/CherryBlossomPose.jpg';
+import pic4 from '../assets/StageImage.jpg';
+import Image from 'react-bootstrap/Image';
+
+function Slideshow() {
+  return (
+    <Carousel fade>
+      <Carousel.Item>
+        <Image src={pic1} fluid />;
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <Image src={pic2} fluid />;
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <Image src={pic3} fluid />;
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+}
+
+export default Slideshow;
